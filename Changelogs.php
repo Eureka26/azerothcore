2c13d23eeb5868993b5fcaa1289af61070ea7f7a
Eureka26 <damiansp10@gmail.com>
2025-03-24 15:57:37 -0300

    Create Changelogs.php

---
+++ Changelogs.php
@@ -0,0 +1,21 @@
+SERVERALERT:
+<html><body>
+<h1 align="center">Parche 0.1 Beta</h1>
+<img src="./announcementslk.php_files/NoticiaLogin4110" width="270" height="15" align="center"/>
+<p>24/3/2025 15:55</p>
+<h2>|cff00ffffChangelogs|r |cff8080ff1|r</h2>
+<h3>Added Store In-Game.<br /><br /></h3>
+<h3>Added Transmog System AIO.<br /><br /></h3>
+<h3>Added New Items from Cata up to Legion.<br /><br /></h3>
+<h3>Added Transmog System AIO.<br /><br /></h3>
+<h3>Added Store In-Game.<br /><br /></h3>
+<h2><br />Mi Facebook <a href="https://www.facebook.com/Oscar.SP.26/">|cffff0000 Eureka26|r |cff00ff00</a></h2>
+<p>|cffff00ff*</p>
+<p>|cffff00ff*</p>
+<p>|cffff00ff*</p>
+<h1 align="center"> |TInterface/Icons/hw.exe:16:16:0:-5:64:64:4:60:4:60|tChallenge-Project</h1>
+<p>|cffff00ff*</p>
+<p>|cffff00ff*</p>
+<p>|cffff00ff*</p>
+
+</body></html>
